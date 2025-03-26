5e5d4db85158a6b3845149e45f69aefe30c15ae2
FacundoPeralta <facundoperalta217@gmail.com>
2025-03-26 00:23:45 -0300

    solucionado el error

---
+++ tp1_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int numCuadrado(int num1);
+void numCuadrado2(int *num2);
+
+
+int main(int argc, char const *argv[])
+{
+    int num1,num3, num2;
+    
+
+
+    printf("Ingrese dos numeros");
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+
+    int *punt=&num2;
+
+    num3=numCuadrado(num1);
+    printf("El cuadrado del primer numero es: %d\n",num3);
+    
+    numCuadrado2(&num2);
+    printf("El cuadrado del segundo numero es: %d",*punt);
+
+    return 0;
+}
+
+int numCuadrado(int num1){
+int num2=num1*num1;
+
+return num2;
+
+}
+void numCuadrado2(int *num2){
+
+    *(num2)*=*num2;
+}
